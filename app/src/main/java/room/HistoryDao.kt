bfc3f2a5ce7138992b71990a6210225ff52f1e08
EvgenNaumov <naumovev@mail.ru>
2022-04-28 17:16:34 +0300

    урок 8. работа с SQLite

---
+++ app/src/main/java/room/HistoryDao.kt
@@ -0,0 +1,24 @@
+package room
+
+import androidx.room.*
+
+@Dao
+interface HistoryDao {
+    @Query("INSERT INTO history_table (city,temperature,feelsLike,icon) VALUES(:city,:temperature,:fellsLike,:icon)")
+    fun nativeInsert()
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    fun insert(entity: HistoryEntity)
+
+    @Delete
+    fun delete(entity: HistoryEntity)
+
+    @Update
+    fun update(entity: HistoryEntity)
+
+    @Query("SELECT * FROM history_table")
+    fun getAll():List<HistoryEntity>
+
+    @Query("SELECT * FROM history_table WHERE city=:city")
+    fun getHistoryForCity(city:String):List<HistoryEntity>
+}
---
+++ app/src/main/java/room/HistoryEntity.kt
@@ -0,0 +1,18 @@
+package room
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import repository.City
+import repository.getDefaultCity
+
+@Entity(tableName = "history_table")
+data class HistoryEntity (
+    @PrimaryKey(autoGenerate = true)
+    val id:Long,
+    var city: String,
+    val temperature: Int,
+    val feelsLike: Int,
+    val icon: String,
+    val condition: String = "cloudy"
+
+)
